1c3c5ede1970696a833bcbc1ed9126e3a876cefd
lun <huazhi.lun@sjtu.edu.cn>
2020-03-14 19:45:18 +0800

    xixi

---
+++ simulate_data.py
@@ -0,0 +1,14 @@
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import wait
+from funtions import generate_signal
+from funtions import generate_anomaly_wss
+
+# with ProcessPoolExecutor(5) as executor:
+#     for _ in range(5):
+#         res.append(executor.submit(hanshu,(1)))
+#     wait(res)
+#     time.sleep(1)
+#     print('xixi')
+
+
+ssss
